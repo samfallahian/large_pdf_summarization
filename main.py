622dc60e42edf9a034a0e943f4043dacd80c4d08
samfallahian <mal.fallahian@gmail.com>
2024-10-29 15:45:24 -0400

    fix: summarization call back function.

--- main.py
+++ main.py
@@ -22,25 +22,30 @@ with gr.Blocks() as demo:
         summary_output_m = gr.Markdown("## Your summary will appear here.")
     with gr.Row():
         with gr.Column(scale=1):
-            summarization_type = gr.Radio(
-                ["Map-Reduce", "Iterative Refinement"],
-                value="Map-Reduce",
-                label="Select Summarization Type",
-                info="Choose summarization method.",
-            )
-            ollama_model = gr.Dropdown(
-                get_ollama_model_list(),
-                label="Ollama Model",
-                info="Select Ollama Model",
-            )
+            with gr.Row():
+                summarization_type = gr.Radio(
+                    ["Map-Reduce", "Iterative Refinement"],
+                    value="Map-Reduce",
+                    label="Select Summarization Type",
+                    info="Choose summarization method.",
+                )
+                ollama_model = gr.Dropdown(
+                    get_ollama_model_list(),
+                    label="Ollama Model",
+                    info="Select Ollama Model",
+                )
+            with gr.Row():
+                chunk_size = gr.Number(
+                    label="Document Chunk Size", value=1000, precision=0
+                )
+                max_token = gr.Number(
+                    label="Maximum Token Length", value=1000, precision=0
+                )
+            with gr.Row():
+                summarize_button = gr.Button("Summarize")
         with gr.Column(scale=1):
-            chunk_size = gr.Number(
-                label="Document Chunk Size", value=1000, precision=0
-            )
-            max_token = gr.Number(
-                label="Maximum Token Length", value=1000, precision=0
-            )
-    summarize_button = gr.Button("Summarize")
+            graph_image = gr.Image(type='pil', interactive=False, label="Workflow:", height=300)
+    # summarize_button = gr.Button("Summarize")
 
     summarize_button.click(
         fn=summarize_pdf,
@@ -51,7 +56,7 @@ with gr.Blocks() as demo:
             chunk_size,
             max_token,
         ],
-        outputs=[summary_output_m, summary_output],
+        outputs=[summary_output_m, summary_output, graph_image],
     )
 
     Log(LOG_FILE, dark=True)
